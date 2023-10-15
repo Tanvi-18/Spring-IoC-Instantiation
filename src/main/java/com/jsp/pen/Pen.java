96960309bb3dde028eb678786d5aa26d3905e373
Tanvi-18 <tanvi18parab@gmail.com>
2023-10-15 22:38:46 +0530

    Instantiation using Spring-Ioc

---
+++ src/main/java/com/jsp/pen/Pen.java
@@ -0,0 +1,15 @@
+package com.jsp.pen;
+
+public class Pen {
+	
+	Pen(){
+		System.out.println("Pen object created");
+	}
+	
+	//nsm
+	public void write() {
+		System.out.println("Pen writes...");
+	}
+
+
+}
